c75abc47a45e993270d18359eb599cbf42367a13
Escudero <claudioe@gmail.com>
2020-12-10 00:12:57 -0300

    Fixing

--- PascalVOC_to_YOLOV3.py
+++ PascalVOC_to_YOLOV3.py
@@ -6,13 +6,13 @@ import sys, getopt
 
 names = []
 
-def ParseXML(xml_path, file):
+def ParseXML(pascalvoc_path, file):
     global names
-    for xml_file in glob(os.path.join(xml_path, '*.xml')):
+    for xml_file in glob(os.path.join(pascalvoc_path, 'Annotations', '*.xml')):
         tree = ET.parse(open(xml_file))
         root = tree.getroot()
         image_name = root.find('filename').text
-        img_path = os.path.join(xml_path, image_name)
+        img_path = os.path.join(pascalvoc_path, image_name)
         for i, obj in enumerate(root.iter('object')):
             klass_name = obj.find('name').text
             if klass_name not in names:
@@ -29,9 +29,9 @@ def ParseXML(xml_path, file):
         print(img_path)
         file.write(img_path+'\n')
 
-def run_PascalVOC_to_YOLOV3(xml_path, yolo_file, names_file):
+def run_PascalVOC_to_YOLOV3(pascalvoc_path, yolo_file, names_file):
     with open(yolo_file, "w") as file:
-        ParseXML(xml_path, file)
+        ParseXML(pascalvoc_path, file)
 
     print("Names:", names)
     with open(names_file, "w") as file:
@@ -41,25 +41,25 @@ def run_PascalVOC_to_YOLOV3(xml_path, yolo_file, names_file):
 if __name__ == '__main__':
     argv = sys.argv[1:]
     try:
-        opts, args = getopt.getopt(argv, 'h', ['xmlpath=', 'yolofile=', 'namesfile='])
+        opts, args = getopt.getopt(argv, 'h', ['pascalvocpath=', 'yolofile=', 'namesfile='])
     except getopt.GetoptError:
-        print('PascalVOC_to_YOLOV3.py --xmlpath <xmlpath> --yolofile <yolofile> --namesfile <namesfile>')
+        print('PascalVOC_to_YOLOV3.py --pascalvocpath <pascalvocpath> --yolofile <yolofile> --namesfile <namesfile>')
         sys.exit(2)
-    xml_path, yolo_file, names_file = [None, None, None]
+    pascalvoc_path, yolo_file, names_file = [None, None, None]
     for opt, arg in opts:
         if opt == '-h':
-            print('PascalVOC_to_YOLOV3.py --xmlpath <xmlpath> --yolofile <yolofile> --namesfile <namesfile>')
+            print('PascalVOC_to_YOLOV3.py --pascalvocpath <pascalvocpath> --yolofile <yolofile> --namesfile <namesfile>')
             sys.exit()
-        elif opt in ("--xmlpath"):
-            xml_path = arg
+        elif opt in ("--pascalvocpath"):
+            pascalvoc_path = arg
         elif opt in ("--yolofile"):
             yolo_file = arg
         elif opt in ("--namesfile"):
             names_file = arg
-    if None in [xml_path, yolo_file, names_file]:
-        print('PascalVOC_to_YOLOV3.py --xmlpath <xmlpath> --yolofile <yolofile> --namesfile <namesfile>')
+    if None in [pascalvoc_path, yolo_file, names_file]:
+        print('PascalVOC_to_YOLOV3.py --pascalvocpath <pascalvocpath> --yolofile <yolofile> --namesfile <namesfile>')
         sys.exit()
 
-    run_PascalVOC_to_YOLOV3(xml_path, yolo_file, names_file)
+    run_PascalVOC_to_YOLOV3(pascalvoc_path, yolo_file, names_file)
 
     
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Used to convert PASCAL VOC 1.1 (XML) labels to YOLOv3 training labels
 
 ## Example:
-PascalVOC_to_YOLOV3.py --xmlpath &lt;xmlpath&gt; --yolofile &lt;yolofile&gt; --namesfile &lt;namesfile&gt;
+PascalVOC_to_YOLOV3.py --pascalvocpath &lt;pascalvocpath&gt; --yolofile &lt;yolofile&gt; --namesfile &lt;namesfile&gt;
 
-PascalVOC_to_YOLOV3.py --xmlpath datasets/iris/xml/ --yolofile datasets/iris/data.txt --namesfile datasets/iris/data.names
+PascalVOC_to_YOLOV3.py --pascalvocpath datasets/iris/ --yolofile datasets/iris/data.txt --namesfile datasets/iris/data.names
